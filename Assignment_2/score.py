c41d7c0ec40a47cfbda098868c7b33add20d26ff
srini <s.pr25@yahoo.com>
2023-03-16 00:15:48 +0530

    Score done

--- Assignment_2/score.py
+++ Assignment_2/score.py
@@ -1,7 +1,7 @@
 from textblob import TextBlob
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 import pandas as pd
-import numpy as np
+import subprocess
 import csv
 import mlflow
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
@@ -12,14 +12,16 @@ def split_into_lemmas(message):
     words = TextBlob(message).words
     return [word.lemma for word in words]
 
-
-best_model = mlflow.sklearn.load_model("runs:/5c18105aa4f34a6188cf0da1dbd3f962/SVM_69")
-messages = pd.read_csv('data/train.csv', sep='\t', quoting=csv.QUOTE_NONE, index_col=False)
-bow_transformer = CountVectorizer(analyzer=split_into_lemmas).fit(messages['message'])
-messages_bow = bow_transformer.transform(messages['message'])
-tfidf_transformer = TfidfTransformer().fit(messages_bow)
-messages_tfidf = tfidf_transformer.transform(messages_bow)
-ip = ["Sure Shot INTRADAY & MULTIBAGGER Stock Tips - Earn 120% PROFIT in 4 Month https://bit.ly/NSE_7 - Click on Link & Send 'JOIN FREE' Message on WhatsApp EXPTRADE"]
-valid_bow = bow_transformer.transform(ip)
-valid_tfidf = tfidf_transformer.transform(valid_bow)
-print(best_model.predict_proba(valid_tfidf))
+def score(text, model, threshold = 0.5):
+    subprocess.Popen('mlflow ui', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    best_model = mlflow.sklearn.load_model(model)
+    messages = pd.read_csv('data/train.csv', sep='\t', quoting=csv.QUOTE_NONE, index_col=False)
+    bow_transformer = CountVectorizer(analyzer=split_into_lemmas).fit(messages['message'])
+    messages_bow = bow_transformer.transform(messages['message'])
+    tfidf_transformer = TfidfTransformer().fit(messages_bow)
+    valid_bow = bow_transformer.transform([text])
+    valid_tfidf = tfidf_transformer.transform(valid_bow)
+    propensity = best_model.predict_proba(valid_tfidf)
+    prediction = propensity[0,1] > threshold
+    subprocess.Popen('pkill -f gunicorn', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    return prediction, propensity[0,1]
---
+++ Assignment_2/test.py
@@ -0,0 +1,38 @@
+from score import score
+import numpy as np
+
+def test_score():
+    #Obvious spam
+    ip = "Sure Shot INTRADAY & MULTIBAGGER Stock Tips - Earn 120% PROFIT in 4 Month https://bit.ly/NSE_7 - Click on Link & Send 'JOIN FREE' Message on WhatsApp EXPTRADE"
+    model = "runs:/5c18105aa4f34a6188cf0da1dbd3f962/SVM_69"
+    threshold = 0.7
+    prediction, propensity = score(ip, model, threshold)
+    print(prediction, propensity)
+    assert prediction == True
+    
+    #Smoke test
+    assert prediction != None
+    assert propensity != None
+
+    #Format test
+    assert type(propensity) == np.float64
+    assert type(prediction) == np.bool_
+
+    #Make sure the values fall in the expected domain of values
+
+    assert prediction == True or prediction == False
+    assert propensity >= 0 and propensity <= 1
+
+    #Threshold of 0 always labels spam
+    prediction, propensity = score(ip, model, 0)
+    assert prediction == True
+
+    #Threshold of 1 always labels not spam
+    prediction, propensity = score(ip, model, 1)
+    assert prediction == False
+
+    #Obvious not spam
+    prediction, propensity = score("hi, wassup?", model, 0.5)
+    assert prediction == False
+
+test_score()
